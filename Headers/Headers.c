63293ce591839cd9c9728b593e09d96fdacfacd5
Max Rodriguez <maxrcoppola@gmail.com>
2021-02-26 09:23:02 -0800

    Headers & Makefile

--- Headers/Headers.c
+++ Headers/Headers.c
@@ -1,3 +1,22 @@
+/* -----------------------------------------
+	Example of Using a Header File in C
+	This header file defines boolean values
+*/
+
+#include "include/header.h"
+
 #include <stdio.h>
 #include <stdlib.h>
-#include "Header.h"
+
+int main() {
+
+	// Use defined macros from header
+	boolean bool1 = true;
+	boolean bool2 = false;
+
+	printf("Boolean 1 Value: %i\n", bool1);
+	printf("Boolean 2 Value: %i\n", bool2);
+
+	return EXIT_SUCCESS;
+
+}
---
+++ Headers/include/header.h
@@ -0,0 +1,18 @@
+/* ------------------------
+	Example Header File
+	Defines Boolean Values
+*/
+
+// Avoid redefining twice
+#ifndef INCLUDED
+
+	#define INCLUDED
+
+	#define true 1
+	#define false 0
+
+	typedef enum {
+		TRUE, FALSE
+	} boolean;
+
+#endif
